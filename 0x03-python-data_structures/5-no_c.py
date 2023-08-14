d9b8abc220295adc81c62dd8d6ccbd57629e002f
Youssef-At <youssef.ataifa@hotmail.comclear>
2023-08-14 15:57:01 -0700

    pyth

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
-
-    string_without_c = ""
-
-    for c in my_string:
-        if c != 'c' and c != 'C':
-            string_without_c += c
-
-    return(string_without_c)
+    if my_string is not None:
+        return ''.join(filter(lambda c: c.lower() != 'c', my_string))
+    return None
