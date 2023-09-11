123f1fcb83bd1b32553c19e9a1dce4cd65fca561
Youssef-At <youssef.ataifa@hotmail.comclear>
2023-09-11 15:41:46 -0700

    me

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains one class
+"""
+
+
+class BaseGeometry:
+    """
+    empty class
+
+    """
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains one class, based on 5-base-geometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class contains one public instance method:
+    `def area(self)`
+    """
+
+    def area(self):
+        """
+        public method to determine area
+        Raises:
+        Exception with the message
+        """
+        raise Exception("area() is not implemented")
